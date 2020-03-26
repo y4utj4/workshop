c044096c3ed09dc98a7bc0b43cf2d64a5c3d4d7b
y4utj4 <jschoeneman@outlook.com>
2020-03-26 14:24:03 -0400

    added timeline.py

--- python/timeline.py
+++ python/timeline.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+#################################################
+'''
+This script is meant to create a visual timeline
+for red/blue team activities during a pentest. 
+
+This script requires a spreadsheet with the following format:
+Red Action, Time(mm/dd/yyyy hh:mm:ss), Blue Action, Time(mm/dd/yyyy hh:mm:ss)
+
+Once that's done, just point the script to the file using the -i/--infile switch 
+and you're golden.
+
+Author: Jeremy Schoeneman
+
+'''
 import argparse
 import csv
 import sys
@@ -14,12 +28,14 @@ def main():
     parser.add_argument('-i', '--infile', help='input file for script to process')
     args = parser.parse_args()
 
-
+    # Sets up lists this script uses
     red_action = []
     red_dates = []
     blue_action = []
     blue_dates = []
 
+    # Reads the data from the CSV file
+    # Skips header line
     with open(args.infile, 'r') as csv_file:
         r = csv.reader(csv_file, delimiter=',')
         next(r)
@@ -28,42 +44,49 @@ def main():
             red_dates.append(lines[1])
             blue_action.append(lines[2])
             blue_dates.append(lines[3])
+
+    # Clears out blank list entries then formats them into datetime format
     blue_action = filter(None, blue_action)
     blue_dates = filter(None, blue_dates)
-
     red_dates = [datetime.strptime(d, "%m/%d/%Y %H:%M:%S") for d in red_dates]
     blue_dates = [datetime.strptime(d, "%m/%d/%Y %H:%M:%S") for d in blue_dates]
 
+    # Sets levels for the timeline (the highs and low points for the data)
     levels = np.tile([-1,1,-3,3,-5,5,-7,7,-9,9,-11,11, -13,13],
                  int(np.ceil(len(red_dates)/6)))[:len(red_dates)]
+
+    # Blues set to even numbers to not overlap the red ones
     blue_levels = np.tile([-2,2,-4,4,-6,6,-8,8],
                  int(np.ceil(len(red_dates)/6)))[:len(red_dates)]
-
+    # Adding a bit of style
     matplotlib.rcParams['font.size'] = 9.5
     plt.style.use('dark_background')
-    # Create figure and plot a stem plot with the date
+ 
+    # Creates the timeline
     fig, ax = plt.subplots(figsize=(18, 8), constrained_layout=True)
 
+    # Titles the timeline
     ax.set_title("Internal Pentest Timeline", fontsize=20)
     ax.set_xlabel('Date', fontsize=20)
 
+    # Makes the center line and plot lines
     markerline, stemline, baseline = ax.stem(red_dates, levels,
-                                             linefmt="#FFFFFF", basefmt="white",
-                                             use_line_collection=True)
-    
+                                             linefmt="#666666", basefmt="white",
+                                             use_line_collection=True) 
     plt.setp(markerline, mec="k", mfc="w", zorder=3)
     
-    # Shift the markers to the baseline by replacing the y-data by zeros.
+    # Shift the markers to the baseline.
     markerline.set_ydata(np.zeros(len(red_dates)))
     
-    # annotate lines
+    # Creates the plot points
     vert = np.array(['top', 'bottom'])[(levels > 0).astype(int)]
- 
+    # red plot points
     for d, l, r, va in zip(red_dates, levels, red_action, vert):
         ax.annotate(r, xy=(d, l), xytext=(-3, np.sign(l)*3),
                     textcoords="offset points", va=va, ha="center", 
                     bbox=dict(boxstyle="round", fc=("#b30000"), ec=("black")))
-
+    
+    # blue plot points
     for d, l, r, va in zip(blue_dates, blue_levels, blue_action, vert):
         ax.annotate(r, xy=(d, l), xytext=(-3, np.sign(l)*3),
                     textcoords="offset points", va=va, ha="center", 
@@ -81,5 +104,6 @@ def main():
     
     ax.margins(y=0.1)
     plt.show()
+
 if __name__ == '__main__':
     main()
